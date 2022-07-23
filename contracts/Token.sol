50f90f853afe9853aa01d686dbae5fac29addfc4
edmondhabimana <edmond.habimana@yahoo.com>
2022-07-22 17:48:24 -0700

    6. Token Approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,8 +12,14 @@ contract Token {
 
     //Track Balances
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     constructor(
         string memory _name,
@@ -40,6 +46,16 @@ contract Token {
         balanceOf[_to] = balanceOf[_to] + _value;
         //Emit Evens
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 };
 
 describe("Token", () => {
-  let token, accounts, deployer, receiver;
+  let token, accounts, deployer, receiver, exchange;
 
   beforeEach(async () => {
     //Fetch Token contract
@@ -17,6 +17,7 @@ describe("Token", () => {
     accounts = await ethers.getSigners();
     deployer = accounts[0];
     receiver = accounts[1];
+    exchange = accounts[2];
   });
 
   describe("Deployment", () => {
@@ -56,6 +57,7 @@ describe("Token", () => {
     describe("Success", () => {
       beforeEach(async () => {
         amount = tokens(100);
+
         transaction = await token
           .connect(deployer)
           .transfer(receiver.address, amount);
@@ -77,6 +79,66 @@ describe("Token", () => {
         expect(args.from).to.equal(deployer.address);
         expect(args.to).to.equal(receiver.address);
         expect(args.value).to.equal(amount);
+      });
+    });
+
+    describe("failure", () => {
+      it("rejects insufficient balances", async () => {
+        //Transfer more tokens than deployer has - 100M
+        const invalidAmount = tokens(100000000);
+        await expect(
+          token.connect(deployer).transfer(receiver.address, invalidAmount)
+        ).to.be.reverted;
+      });
+
+      it("Reject invalid recipent", async () => {
+        const amount = tokens(100);
+        await expect(
+          token
+            .connect(deployer)
+            .transfer("0x0000000000000000000000000000000000000000", amount)
+        ).to.be.reverted;
+      });
+    });
+  });
+
+  describe("Approving Tokens", () => {
+    let amount, transaction, result;
+
+    beforeEach(async () => {
+      amount = tokens(100);
+
+      transaction = await token
+        .connect(deployer)
+        .approve(exchange.address, amount);
+      result = await transaction.wait();
+    });
+
+    describe("Success", () => {
+      it("allocate an allowance for delegated token spending", async () => {
+        expect(
+          await token.allowance(deployer.address, exchange.address)
+        ).to.equal(amount);
+      });
+
+      it("emits a Transfer event", async () => {
+        const event = result.events[0];
+        expect(event.event).to.equal("Approval");
+
+        const args = event.args;
+        expect(args.owner).to.equal(deployer.address);
+        expect(args.spender).to.equal(exchange.address);
+        expect(args.value).to.equal(amount);
+      });
+
+      describe("Failure", () => {
+        it("rejects invalid spenders", async () => {
+          await expect(
+            token
+              .connect(deployer)
+              .approve("0x0000000000000000000000000000000000000000", amount)
+          ).to.be.reverted;
+        });
       });
     });
   });
