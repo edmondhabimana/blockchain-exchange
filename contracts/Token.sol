6ab4d29aabcbd73ce444110fa3392ccae42ff41b
edmondhabimana <edmond.habimana@yahoo.com>
2022-07-24 11:49:56 -0700

    7. Token Transfer Delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -38,15 +38,25 @@ contract Token {
     {
         //Require that sender has enough tokens to spend
         require(balanceOf[msg.sender] >= _value);
+
+        _transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
         require(_to != address(0));
 
         //Deduct tokens from spender
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_from] = balanceOf[_from] - _value;
         //Credit tokens to reciever
         balanceOf[_to] = balanceOf[_to] + _value;
         //Emit Evens
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(address _spender, uint256 _value)
@@ -56,6 +66,21 @@ contract Token {
         require(_spender != address(0));
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        //chech approval
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+        //spend token
+        _transfer(_from, _to, _value);
         return true;
     }
 }
